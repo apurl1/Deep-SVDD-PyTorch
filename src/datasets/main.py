2a368bab8fb1b2994a41c7f269a139707860d2ca
Rupal Nigam <rupaln@Rupals-MacBook-Pro.local>
2020-11-16 11:12:28 -0700

    changes to train on fashion dataset

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -1,17 +1,21 @@
 from .mnist import MNIST_Dataset
+from .fashion import Fashion_Dataset
 from .cifar10 import CIFAR10_Dataset
 
 
 def load_dataset(dataset_name, data_path, outlier_class):
     """Loads the dataset."""
 
-    implemented_datasets = ('mnist', 'cifar10')
+    implemented_datasets = ('mnist', 'fashion')
     assert dataset_name in implemented_datasets
 
     dataset = None
 
     if dataset_name == 'mnist':
         dataset = MNIST_Dataset(root=data_path, outlier_class=outlier_class)
+    
+    if dataset_name == 'fashion':
+        dataset = Fashion_Dataset(root=data_path, outlier_class=outlier_class)
 
     #if dataset_name == 'cifar10':
         #dataset = CIFAR10_Dataset(root=data_path, outlier_class=outlier_class)

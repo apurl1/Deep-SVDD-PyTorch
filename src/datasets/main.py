b3ebf25e9882f60723d82ca0eab2b520efb3135d
Rupal Nigam <rupaln@Rupals-MacBook-Pro.local>
2021-01-14 21:16:59 -0700

    add cifar10 compatability

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -6,7 +6,7 @@ from .cifar10 import CIFAR10_Dataset
 def load_dataset(dataset_name, data_path, outlier_class):
     """Loads the dataset."""
 
-    implemented_datasets = ('mnist', 'fashion')
+    implemented_datasets = ('mnist', 'fashion', 'cifar10')
     assert dataset_name in implemented_datasets
 
     dataset = None
@@ -17,7 +17,7 @@ def load_dataset(dataset_name, data_path, outlier_class):
     if dataset_name == 'fashion':
         dataset = Fashion_Dataset(root=data_path, outlier_class=outlier_class)
 
-    #if dataset_name == 'cifar10':
-        #dataset = CIFAR10_Dataset(root=data_path, outlier_class=outlier_class)
+    if dataset_name == 'cifar10':
+        dataset = CIFAR10_Dataset(root=data_path, outlier_class=outlier_class)
 
     return dataset
